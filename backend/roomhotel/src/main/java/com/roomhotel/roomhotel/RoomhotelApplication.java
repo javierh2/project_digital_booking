17667bd5942da6ac98ab28c6354af40ce5953924
javier <javier.morabaiz@gmail.com>
2026-02-25 22:34:47 -0300

    creation of backend and folders in java

---
+++ backend/roomhotel/src/main/java/com/roomhotel/roomhotel/RoomhotelApplication.java
@@ -0,0 +1,13 @@
+package com.roomhotel.roomhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomhotelApplication.class, args);
+	}
+
+}
