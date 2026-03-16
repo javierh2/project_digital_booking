9e09d3b314a2874560d887ac7ee57ec3b090f5d6
javier <javier.morabaiz@gmail.com>
2026-03-16 13:33:36 -0300

    new exceptions handling validation and credentials

--- backend/roomhotel/src/main/java/com/roomhotel/roomhotel/exception/GlobalExceptionHandler.java
+++ backend/roomhotel/src/main/java/com/roomhotel/roomhotel/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.roomhotel.roomhotel.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.MethodNotAllowedException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -65,5 +67,25 @@ public class GlobalExceptionHandler {
         response.put("message", message);
 
         return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(MethodNotAllowedException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(
+            MethodArgumentNotValidException exception
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(),error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String,String>> handleBadCredentials(
+            BadCredentialsException exception
+    ){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("error", "Email o contraseña incorrectos"));
     }
 }
